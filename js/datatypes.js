b80c625cf6b9e82e7df17b1e1657f9102bd24a33
apt-getdiana <dianags1811@gmail.com>
2021-10-05 19:07:53 -0500

    js update

---
+++ js/datatypes.js
@@ -0,0 +1,28 @@
+//javascript is an interpreteded, not typed or weakly typed 
+            //and single threaded programming language
+
+            //Types of variables
+            //Number-> var variable = number;
+            var variable = 0;
+            console.log(variable);
+            //String -> var variable = "number";
+            variable = "hola a todos";
+            console.log(variable);
+            // Boolean -> var variable = true | false;
+            variable = true;
+            console.log(variable);
+            /*Object -> var variable = {
+                property: value,
+                property2: value2
+            };*/
+            variable = {};
+            console.log(variable);
+            // array -> var variable = [value, value2, ..., value_n]
+            variable = [];
+            console.log(variable);
+            // null -> var variable = null
+            variable = null;
+            console.log(variable);
+            // undefined -> var variable = undefined
+            variable = undefined;
+            console.log(variable);
