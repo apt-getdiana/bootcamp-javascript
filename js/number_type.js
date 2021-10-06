00b0bf762fd826d2f90c2c1211c11acb3ae47130
apt-getdiana <dianags1811@gmail.com>
2021-10-05 19:23:11 -0500

    new update

---
+++ js/number_type.js
@@ -0,0 +1,9 @@
+var stringvar="Hello";
+//NaN = Not a Number
+console.log(isNaN(stringvar));
+var numbervar=5;
+console.log(isNaN(numbervar))
+//Max value in JS
+console.log(Number.MAX_VALUE);
+//Min Value in JS
+console.log(Number.MIN_VALUE)
---
+++ js/string_type.js
@@ -0,0 +1,7 @@
+var first_str = "First String"
+var second_str = "Second String"
+var third_str = "This is a template"
+
+
+//concatenate
+console.log(first_str + second_str)
